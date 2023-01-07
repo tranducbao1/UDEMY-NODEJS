ab649417c71cece8abcd9cc24ab347b954afd12e
tranducbao1 <19521257@gm.uit.edu.vn>
2023-01-07 16:23:10 +0700

    end part5

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const http = require(`http`);
-//test git
-//test git 2
 const express = require(`express`);
+const bodayParser = require(`body-parser`);
+const path = require(`path`);
 const app = express();
+
+const adminRoutes = require(`./routes/admin`);
+const shopRoutes = require(`./routes/shop`);
+
+app.use(bodayParser.urlencoded());
+app.use(express.static(path.join(__dirname, `public`)));
+
+app.use(`/admin`, adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  console.log(`MIDDLEWARE`);
-  next();
+  res.status(404).sendFile(path.join(__dirname, `views`, `404.html`));
 });
-app.use((req, res, next) => {
-  console.log(`MIDDLEWARE 2`);
-});
-const server = http.createServer(app);
-server.listen(3000);
+
+app.listen(3000);
