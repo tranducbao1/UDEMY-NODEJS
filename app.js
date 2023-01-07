47c1bfd4e04a21b7fdda5f05bfcaa28dc0d2adc1
tdb <tranducbao192@gmail.com>
2023-01-07 12:46:24 +0700

    test middleware

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//aaaa
+const http = require(`http`);
+
+const express = require(`express`);
+const app = express();
+app.use((req, res, next) => {
+  console.log(`MIDDLEWARE`);
+  next();
+});
+app.use((req, res, next) => {
+  console.log(`MIDDLEWARE 2`);
+});
+const server = http.createServer(app);
+server.listen(3000);
