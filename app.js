b7374aa6ed67b78489299a1e4d83578009bbdd41
tranducbao1 <19521257@gm.uit.edu.vn>
2023-01-08 14:22:11 +0700

    using pug

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const bodayParser = require(`body-parser`);
 const path = require(`path`);
 const app = express();
 
-const adminRoutes = require(`./routes/admin`);
+app.set(`view engine`, `pug`);
+app.set(`views`, `views`);
+
+const adminData = require(`./routes/admin`);
 const shopRoutes = require(`./routes/shop`);
 
 app.use(bodayParser.urlencoded());
 app.use(express.static(path.join(__dirname, `public`)));
 
-app.use(`/admin`, adminRoutes);
+app.use(`/admin`, adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
