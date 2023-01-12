f1195db2c251267644fac4dcabe2c34dcccb8d7f
tranducbao1 <19521257@gm.uit.edu.vn>
2023-01-12 13:22:23 +0700

    product detail

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
@@ -32,6 +43,12 @@ exports.getOrders = (req, res, next) => {
     path: "/orders",
     pageTitle: "Your Orders",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
 };
 
 exports.getCheckout = (req, res, next) => {
