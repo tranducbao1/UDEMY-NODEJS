e8fd34525f9d8232937ac52b27553c540664a5df
tranducbao1 <19521257@gm.uit.edu.vn>
2023-01-18 17:08:13 +0700

    understanding sequelize v1

--- util/database.js
+++ util/database.js
@@ -1,10 +1,6 @@
-const mysql = require("mysql2");
-
-const pool = mysql.createPool({
+const Sequelize = require(`sequelize`);
+const sequelize = new Sequelize(`node_complete`, `root`, `congnghethongtin`, {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node_complete",
-  password: "congnghethongtin",
 });
-
-module.exports = pool.promise();
+module.exports = sequelize;
