30dc4421b3c785c4135feda026b60b2840d0cbc6
tranducbao1 <19521257@gm.uit.edu.vn>
2023-01-31 14:05:33 +0700

    cart

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoConnect = require("./util/database").mongoConnect;
+
 const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,19 +19,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
-  next();
+  User.findById("63d8a33ce0eaacff705ef64f")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
 mongoConnect(() => {
   app.listen(3000);
 });
--- models/product.js
+++ models/product.js
@@ -1,13 +1,16 @@
-const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
+
   save() {
     const db = getDb();
     let dbOp;
@@ -27,6 +30,7 @@ class Product {
         console.log(err);
       });
   }
+
   static fetchAll() {
     const db = getDb();
     return db
@@ -41,6 +45,7 @@ class Product {
         console.log(err);
       });
   }
+
   static findById(prodId) {
     const db = getDb();
     return db
@@ -55,6 +60,7 @@ class Product {
         console.log(err);
       });
   }
+
   static deleteById(prodId) {
     const db = getDb();
     return db
@@ -68,5 +74,5 @@ class Product {
       });
   }
 }
-//
+
 module.exports = Product;
