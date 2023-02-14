e8eaeb60dd720c4c803823491043f00b5f887de4
tranducbao1 <19521257@gm.uit.edu.vn>
2023-02-14 08:34:25 +0700

    update

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const errorController = require("./controllers/error");
 const session = require("express-session");
-
+const MongoDBStore = require("connect-mongodb-session")(session);
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://19521257:congnghethongtin@mern.b3jpg.mongodb.net/shop?retryWrites=true&w=majority";
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -20,7 +27,12 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
-  session({ secret: "my secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
@@ -39,9 +51,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://19521257:congnghethongtin@mern.b3jpg.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
